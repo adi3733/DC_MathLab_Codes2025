c61f13eb633e1e920a4d351fde66622d2318909c
adi3733 <adi.ghayal23@gmail.com>
2025-10-27 21:27:31 +0530

    updated 10 with encoding & decoding

--- assets/Adi10.m
+++ assets/Adi10.m
@@ -1,3 +1,4 @@
+======== ENCODING PRORAM=========
 clc;
 clear all;
 close all;
@@ -17,3 +18,18 @@ cv2=str2num(dec2base(bin2dec(c),8));
 cv=[cv1,cv2]
 trellis=poly2trellis(k,cv)
 code=convenc(msg,trellis)
+
+========DECODING PRORAM=========
+clc;
+clear;
+disp('Aditya R Ghayal ROLL NO: 23');
+msg=input('enter the msg sequence=');
+display(msg);
+a=str2num(dec2base(bin2dec('111'),8));
+b=str2num(dec2base(bin2dec('110'),8));
+g=[a b];
+trellis=poly2trellis(3,g);
+code=convenc(msg,trellis)
+tblen=1;
+decoded=vitdec(code,trellis,tblen,'trunc','hard')
+
